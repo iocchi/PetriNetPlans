a3717561d633e442a557594f68ab383ae1a43e05
Julia Nitsch <julia.nitsch@student.tugraz.at>
2014-08-22 15:46:13 +0200

    deleted qtcreator dirs

--- PNPros/ROS_bridge/pnp_ros/qtcreator-build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/jnitsch/ros_hydro/pnp_ws/src/petri_net/pnp_ros/qtcreator-build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
