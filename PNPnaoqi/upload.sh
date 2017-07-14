4ac68da76197c3b60e2ec8cde790462db4cc93f4
Luca Iocchi <iocchi@dis.uniroma1.it>
2017-07-14 12:02:58 +0200

    cmake flex

--- PNPnaoqi/upload.sh
+++ PNPnaoqi/upload.sh
@@ -1,7 +1,8 @@
-scp build-pepper/sdk/lib/libpnp.so nao@$PEPPER_IP:spqrel/lib
-scp build-pepper/sdk/bin/pnp_test nao@$PEPPER_IP:spqrel/bin
-scp build-pepper/sdk/bin/pnp_naoqi nao@$PEPPER_IP:spqrel/bin
-scp plans/*.pnml nao@$PEPPER_IP:spqrel/plans
-scp plans/run_plan.py nao@$PEPPER_IP:spqrel/plans
-scp actions/*.py nao@$PEPPER_IP:spqrel/actions
+DESTDIR=.
+scp build-pepper/sdk/lib/libpnp.so nao@$PEPPER_IP:$DESTDIR/lib
+scp build-pepper/sdk/bin/pnp_test nao@$PEPPER_IP:$DESTDIR/bin
+scp build-pepper/sdk/bin/pnp_naoqi nao@$PEPPER_IP:$DESTDIR/bin
+scp plans/*.pnml nao@$PEPPER_IP:$DESTDIR/plans
+scp plans/run_plan.py nao@$PEPPER_IP:$DESTDIR/plans
+scp actions/*.py nao@$PEPPER_IP:$DESTDIR/actions
 
