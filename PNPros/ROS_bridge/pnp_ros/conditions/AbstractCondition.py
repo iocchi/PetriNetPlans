7830489e8ae4fb543f56401d6d31d904ff33da12
francescodelduchetto <francescodelduchetto@gmail.com>
2017-11-15 11:41:39 +0000

    fix bug in AbstractCondition.py

--- PNPros/ROS_bridge/pnp_ros/conditions/AbstractCondition.py
+++ PNPros/ROS_bridge/pnp_ros/conditions/AbstractCondition.py
@@ -8,8 +8,11 @@ class AbstractCondition():
         # subscribe to the topic with a callback
         rospy.Subscriber(self._topic_name, self._topic_type, self._callback)
 
+        # last_data will be None until the subscribed topic will return some data
+        self.last_data = None
+
     def _callback(self, data):
-        self.last_data = data.data
+        self.last_data = data
 
     @abstractproperty
     def _topic_name(self):
