f3e7da43df44d9d2e21a88fa8130cb4ab34fe1aa
Luca Iocchi <iocchi@dis.uniroma1.it>
2018-04-22 15:07:43 +0200

    names.h for ROS topics and params

---
+++ PNPros/ROS_bridge/pnp_ros/include/pnp_ros/names.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// Topics
+
+#define TOPIC_PNPACTIVEPLACES "pnp/currentActivePlaces" // names of current active places
+
+#define TOPIC_PNPCONDITION "PNPConditionEvent" // PNP Conditions as events
+
+#define TOPIC_PLANTOEXEC "planToExec" // executes the plan
+
+#define TOPIC_PNPACTION "pnp_action" // action proxy publisher when using messages 
+// (not used when PNP uses actionlib protocol - default)
+
+#define TOPIC_PNPACTIONCMD "PNPActionCmd" // executes an action command. 
+// Message syntax: <actionname>[_<params>] <start|end|interrupt>
+// TODO Uniform with naoqi
+// Message syntax: [start|end|interrupt] <actionname> [<params>]
+
+
+#define TOPIC_PNPACTIONTERMINATION "pnp_action_termination"  // notifies action is terminated
+
+
+// Params
+#define PARAM_PNPCONDITIONBUFFER "PNPconditionsBuffer/" // PNP Conditions as param values
+
+
+
+
