74807234a3ba26080c6710cbeb3f16dd1497337c
Luca Iocchi <iocchi@diag.uniroma1.it>
2020-04-08 13:06:48 +0200

    docker 0.1

---
+++ docker/1604/run.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Use  ./run.bash [version]
+
+IMAGENAME=ub1604_kinetic_pnp
+
+VERSION=0.1
+if [ ! "$1" == "" ]; then
+  VERSION=$1
+fi
+
+# change setings here if needed
+PNP_HOME=$HOME/src/PetriNetPlans
+PLAYGROUND_FOLDER=$HOME/playground
+
+
+echo "Running image $IMAGENAME:$VERSION ..."
+
+if [ -d /usr/lib/nvidia-384 ]; then
+  NVIDIA_STR="-v /usr/lib/nvidia-384:/usr/lib/nvidia-384 \
+           -v /usr/lib32/nvidia-384:/usr/lib32/nvidia-384 \
+           --device /dev/dri"
+  echo "Nvidia support enabled"
+fi
+
+docker run -it \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/.Xauthority:/home/robot/.Xauthority:rw \
+    $NVIDIA_STR \
+    -e DISPLAY=$DISPLAY \
+    --privileged \
+    --net=host \
+    -v $PLAYGROUND_FOLDER:/home/robot/playground \
+    $IMAGENAME:$VERSION
+
+
---
+++ docker/1804/build.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Use  ./build.bash [Dockerfile] [version]
+
+IMAGENAME=ub1804_melodic_pnp
+
+DOCKERFILE=Dockerfile
+if [ ! "$1" == "" ]; then
+  DOCKERFILE=$1
+fi
+
+VERSION=0.1
+if [ ! "$2" == "" ]; then
+  VERSION=$2
+fi
+
+docker build -t $IMAGENAME:$VERSION -f $DOCKERFILE .
+
