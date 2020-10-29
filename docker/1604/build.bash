b951806eb25c509c725888f53f10cdd099a3bbab
Luca Iocchi <iocchi@diag.uniroma1.it>
2020-10-29 09:52:59 +0100

    no-cache trick to get latest version of PNP at each build; version.txt

--- docker/1604/build.bash
+++ docker/1604/build.bash
@@ -9,13 +9,17 @@ if [ ! "$1" == "" ]; then
   DOCKERFILE=$1
 fi
 
-VERSION=1.0
+VERSION=`cat ../../version.txt`
 if [ ! "$2" == "" ]; then
   VERSION=$2
 fi
 
-docker build --network=host -t $IMAGENAME:base -f Dockerfile.base . && \
-docker build --network=host -t $IMAGENAME:$VERSION -f $DOCKERFILE .
+echo "======================================="
+echo "   Building $IMAGENAME:$VERSION "
+echo "======================================="
 
-docker tag $IMAGENAME:$VERSION $IMAGENAME:latest
+#docker build --network=host -t $IMAGENAME:base -f Dockerfile.base . && \
+#docker build --network=host -t $IMAGENAME:$VERSION -f $DOCKERFILE .
+
+#docker tag $IMAGENAME:$VERSION $IMAGENAME:latest
 
--- docker/1804/build.bash
+++ docker/1804/build.bash
@@ -9,10 +9,14 @@ if [ ! "$1" == "" ]; then
   DOCKERFILE=$1
 fi
 
-VERSION=1.0
+VERSION=`cat ../../version.txt`
 if [ ! "$2" == "" ]; then
   VERSION=$2
 fi
+
+echo "======================================="
+echo "   Building $IMAGENAME:$VERSION "
+echo "======================================="
 
 docker build --network=host -t $IMAGENAME:base -f Dockerfile.base . && \
 docker build --network=host -t $IMAGENAME:$VERSION -f $DOCKERFILE .
