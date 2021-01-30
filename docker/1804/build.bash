68f049b853b8f859693c9273c49b346c15744e16
Luca Iocchi <iocchi@diag.uniroma1.it>
2021-01-30 19:48:53 +0100

    gRPC docker support

--- docker/1804/build.bash
+++ docker/1804/build.bash
@@ -19,7 +19,9 @@ echo "   Building $IMAGENAME:$VERSION "
 echo "======================================="
 
 docker build --network=host -t $IMAGENAME:base -f Dockerfile.base . && \
-docker build --network=host -t $IMAGENAME:$VERSION -f $DOCKERFILE .
+docker build --network=host -t $IMAGENAME:grpc -f $DOCKERFILE.grpc . && \
+docker build --network=host -t ${IMAGENAME}:$VERSION -f $DOCKERFILE .
+
 
 docker tag $IMAGENAME:$VERSION $IMAGENAME:latest
 
